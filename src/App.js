24a46680b6bef72cf2627dfeac2357ea83c3659d
VictorChagas <victorsantoschagas@hotmail.com>
2021-12-31 18:41:58 -0300

    Finalized 95%

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import Home from './pages/Home'
 import Projects from './pages/Projects'
 export default class App extends Component {
   render() {
+    const url = process.env.PUBLIC_URL
     return (
       <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/projects" element={<Projects/>}/>
+            <Route path={url+'/'} element={<Home/>}/>
+            <Route path={url+'/projects'} element={<Projects/>}/>
             <Route path ="*" element={<h1>404</h1>}/>
         </Routes>
       </BrowserRouter>
--- src/pages/Home/components/Main.js
+++ src/pages/Home/components/Main.js
@@ -1,4 +1,4 @@
-import React, { Component,useState } from 'react'
+import React, {useState } from 'react'
 import styled from 'styled-components'
 import principal from '../../../images/principalSVG.png'
 import Icons from '../../../components/Icons'
